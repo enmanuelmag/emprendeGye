1b592ba063ec6e2f40385bcc8ca428a909e78678
enmanuel-mag <fmagalla@fiec.espol.edu.ec>
2020-06-26 00:56:30 -0500

    Add data example of enviroment info

---
+++ src/app/pages/emprendedor/ecosistema/datosEcosistema.js
@@ -0,0 +1,67 @@
+export default {
+	personas: {
+		title: 'Personas con los mismos intereses',
+		items: [
+			{
+				name: 'Kevin Celsius',
+				description: 'Emprendedor y entusiasta de la tecnología',
+			},
+			{
+				name: 'Ana Paula Ortíz',
+				description: 'Marketing digital y community management',
+			},
+			{
+				name: 'Marlon Segura',
+				description: 'En busca de ideas en las cuales invertir',
+			},
+			{
+				name: 'Katherine Romero',
+				description: 'Desarrollador de software senior',
+			},
+		],
+	},
+	grupos_foros: {
+		title: 'Grupos y foros relacionados',
+		items: [
+			{
+				title: 'Pymes del Sur',
+				srcImg: '...',
+				alt: 'Logo de Pymes del sur',
+				type: 'GROUP',
+			},
+			{
+				title: 'Consejos del abuelo emprendedor',
+				srcImg: '...',
+				alt: 'Imagen de una persona',
+				type: 'FORO',
+			},
+			{
+				title: 'Emprendedorres sin límites y sin miedos',
+				srcImg: '...',
+				alt: 'Logo de Emprendedor sin límites y sin miedos',
+				type: 'GRUPO',
+			},
+		],
+	},
+	eventos_talleres: {
+		title: '...',
+		items: [
+			{
+				title: 'Conferencia ÉPICO 2020',
+				description:
+					'Conoce las técnicas y habilidades necesarias para que tu negocio triunfe en esta nueva década.',
+				srcInfo: '...',
+				srcImg: '...',
+				alt: '...',
+			},
+			{
+				title: 'Conferencia Pymes del Sur 2021',
+				description:
+					'Impulsa tu negocio con nuestras sugerencias de nuestros panelistas.',
+				srcInfo: '...',
+				srcImg: '...',
+				alt: '...',
+			},
+		],
+	},
+};
