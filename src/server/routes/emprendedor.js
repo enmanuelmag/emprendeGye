0048f1cfe7fed5fb81ca8f7201b8be1a1f3d52c8
enmanuel-mag <fmagalla@fiec.espol.edu.ec>
2020-08-15 20:46:44 -0500

    Update changes

--- src/server/routes/emprendedor.js
+++ src/server/routes/emprendedor.js
@@ -1,47 +1,56 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
-const emprendedor = require("../models/emprendedor");
+const emprendedor = require('../models/emprendedor');
 
-router.get("/", function(req, res, next) {
-    emprendedor.findAll().then( emprendedor => {
-        if(emprendedor){
-            //console.log(emprendedor);
-            res.json(emprendedor);
-        }else{
-        res.send("No existe ningún emprendedor con ese id");}
-    }).catch(err => {
-        res.send("Error: " + err);
+router.get('/', function (req, res, next) {
+  emprendedor
+    .findAll()
+    .then((emprendedor) => {
+      if (emprendedor) {
+        //console.log(emprendedor);
+        res.json(emprendedor);
+      } else {
+        res.send('No existe ningún emprendedor con ese id');
+      }
     })
-})
+    .catch((err) => {
+      res.send('Error: ' + err);
+    });
+});
 
-
-router.get("/:id", function(req, res, next) {
-    emprendedor.findOne({
-        where: {
-            idEmprendedor: req.params.id
-        }
-    }).then( emprendedor => {
-        if(emprendedor){
-            res.json(emprendedor);
-        }else{
-            console.log("No existe ningún emprendedor con ese id");
-                  }
-    }).catch(err => {
-        res.send("Error: " + err);
+router.get('/:id', function (req, res, next) {
+  emprendedor
+    .findOne({
+      where: {
+        idEmprendedor: req.params.id,
+      },
     })
-})
-
-router.post("/update", function(req, res, next) {
-    
-    emprendedor.update(req.body).then(emprendedor =>{
-        if(emprendedor){
-            res.json(emprendedor);
-        }else{
-            console.log("No se pudo actualizar los datos de emprendedor");
-                  }
-    }).catch(err => {
-        res.send("Error: " + err);
+    .then((emprendedor) => {
+      if (emprendedor) {
+        res.json(emprendedor);
+      } else {
+        console.log('No existe ningún emprendedor con ese id');
+      }
     })
-})
+    .catch((err) => {
+      res.send('Error: ' + err);
+    });
+});
+
+router.post('/update', function (req, res, next) {
+  console.log('Actualziando ', typeof req.body);
+  emprendedor
+    .update(req.body)
+    .then((emprendedor) => {
+      if (emprendedor) {
+        res.json(emprendedor);
+      } else {
+        console.log('No se pudo actualizar los datos de emprendedor');
+      }
+    })
+    .catch((err) => {
+      res.send('Error: ' + err);
+    });
+});
 
 module.exports = router;
--- src/server/server.js
+++ src/server/server.js
@@ -1,49 +1,51 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser');
-
+const { sequelize } = require('./database/db');
 //const httpProxy = require('http-proxy');
 //var index = require("./routes/index");
-var emprendedor = require("./routes/emprendedor");
+var emprendedor = require('./routes/emprendedor');
 
 const PORT = 8080;
 const app = express();
 
 let cors = require('cors');
 
+//CONEXION CON LA BASE DE DATOS EN CLEVER CLOUD
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully :D');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database :(', err);
+  });
+
 app.use(cors());
 //CONFIGURACION DEL SERVER
 
-app.use(bodyParser.json());
-app.use(
-	bodyParser.urlencoded({
-		extended: false,
-	})
-);
+app.use(express.json());
+
 //Especificamos donde estará la caperta de distribuición
 app.use(express.static(path.join(__dirname, '..', '..', 'build')));
 
-
-
-
 app.set('port', process.env.PORT || PORT);
-
 
 //Conexion base de datos
 //app.use("/", index);
-app.use("/emprendedor", emprendedor);
+app.use('/emprendedor', emprendedor);
 
 //Rutas genericas
 app.get('/ping', function (req, res) {
-	console.log('Hola!');
-	return res.send({ data: 'pong' });
+  console.log('Hola!');
+  return res.send({ data: 'pong' });
 });
 
 app.get('/', function (req, res) {
-	res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', '..', 'build', 'index.html'));
 });
 
 //INICIALIZACION DEL SERVIDOR
 app.listen(app.get('port'), () => {
-	console.log(`Server funcionando! ${app.get('port')}`);
+  console.log(`Server funcionando! ${app.get('port')}`);
 });
