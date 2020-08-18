4c72953f08528e2a4c514d86200469aa87694997
dianmoli98 <dianmoli@espol.edu.ec>
2020-08-18 03:08:09 -0500

    Frontend in Reports

--- src/app/pages/admin/layouts/Main/components/Footer/Footer.js
+++ src/app/pages/admin/layouts/Main/components/Footer/Footer.js
@@ -27,13 +27,12 @@ const Footer = props => {
           href="https://devias.io/"
           target="_blank"
         >
-          Devias IO
+          EmprendeGye
         </Link>
-        . 2019
+        . 2020
       </Typography>
       <Typography variant="caption">
-        Created with love for the environment. By designers and developers who
-        love to work together in offices!
+        Creado por el grupo BiTeam
       </Typography>
     </div>
   );
--- src/app/pages/admin/views/Reports/Reports.js
+++ src/app/pages/admin/views/Reports/Reports.js
@@ -1,21 +1,168 @@
-import React, { Fragment } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography as MuiTypography } from '@material-ui/core';
+import React from 'react';
+import SwipeableViews from 'react-swipeable-views';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PropTypes from 'prop-types';
+import TextField from '@material-ui/core/TextField';
+import { Grid, Typography, Card, Container } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    padding: theme.spacing(4)
-  }
-}));
-
-const Reports = () => {
-  const classes = useStyles();
+function TabContainer(props) {
+  const { children, dir } = props;
 
   return (
-    <div className={classes.root}>
-      
-    </div>
+    <Typography component="div" dir={dir} style={{ padding: 8 * 3 }}>
+      {children}
+    </Typography>
   );
-};
+}
 
-export default Reports;
+const styles = theme => ({
+  root: {
+    backgroundColor: theme.palette.background.paper,
+    width: 1127,
+    position: 'relative',
+    minHeight: 200,
+  },
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  title: {
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(1),
+  },
+});
+
+class FloatingActionButtonZoom extends React.Component {
+  state = {
+    value: 0,
+  };
+
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
+  handleChangeIndex = index => {
+    this.setState({ value: index });
+  };
+
+  render() {
+    const { classes, theme } = this.props;
+
+
+    return (
+      <div className={classes.root}>
+      <Card variant='elevation' elevation={2}>
+        <Typography variant="body2" className={classes.title}>
+								 Seleccione el tipo de reporte que desea generar:
+          </Typography>
+          </Card>
+        <AppBar position="static" color="default">
+          <Tabs
+            value={this.state.value}
+            onChange={this.handleChange}
+            indicatorColor="primary"
+            textColor="primary"
+            variant="fullWidth"
+          >
+            <Tab label="REPORTES DE GANANCIAS POR TIPO DE EMPRENDIMIENTO " />
+            <Tab label="TOP DE EMPRENDEDORES CON MAYOR FINANCIAMIENTO" />
+          </Tabs>
+        </AppBar>
+        <SwipeableViews
+          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+          index={this.state.value}
+          onChangeIndex={this.handleChangeIndex}
+        >
+          <TabContainer dir={theme.direction}>
+          <Container className={classes.container} maxWidth={'xl'}>
+              <Grid container spacing={4}>
+            
+                <Grid item xs={12} md={6} lg={6}>
+                  <form className={classes.container} noValidate>
+                    <TextField
+                      id="date"
+                      label="Fecha de inicio"
+                      type="date"
+                      defaultValue="2017-05-24"
+                      className={classes.textField}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </form>
+                </Grid>
+
+                <Grid item xs={12} md={6} lg={6}>
+                  <form className={classes.container} noValidate>
+                    <TextField
+                      id="date"
+                      label="Fecha de fin"
+                      type="date"
+                      defaultValue="2020-02-24"
+                      className={classes.textField}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </form>
+
+                </Grid>
+              </Grid>
+            </Container>
+          </TabContainer>
+
+          <TabContainer dir={theme.direction}>
+            <Container className={classes.container} maxWidth={'xl'}>
+              <Grid container spacing={4}>
+            
+                <Grid item xs={12} md={6} lg={6}>
+                  <form className={classes.container} noValidate>
+                    <TextField
+                      id="date"
+                      label="Fecha de inicio"
+                      type="date"
+                      defaultValue="2017-05-24"
+                      className={classes.textField}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </form>
+                </Grid>
+
+                <Grid item xs={12} md={6} lg={6}>
+                  <form className={classes.container} noValidate>
+                    <TextField
+                      id="date"
+                      label="Fecha de fin"
+                      type="date"
+                      defaultValue="2020-02-24"
+                      className={classes.textField}
+                      InputLabelProps={{
+                        shrink: true,
+                      }}
+                    />
+                  </form>
+
+                </Grid>
+              </Grid>
+            </Container>
+          </TabContainer>
+
+        </SwipeableViews>
+
+      </div>
+    );
+  }
+}
+
+
+export default withStyles(styles, { withTheme: true })(FloatingActionButtonZoom);
