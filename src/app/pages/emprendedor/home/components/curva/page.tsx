0f9cad3bb997b62bb66e48d9b1a0fbaf8e511754
enmanuel-mag <fmagalla@fiec.espol.edu.ec>
2020-08-18 19:19:02 -0500

    Fix problem with curve in dashboard

--- src/app/pages/emprendedor/home/components/curva/page.tsx
+++ src/app/pages/emprendedor/home/components/curva/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Line } from 'react-chartjs-2';
+import chartData from './assets/data';
 import { Card, CardContent, Typography } from '@material-ui/core';
 import style from './style';
 
+const customRadius = (context) => {
+  let index = context.dataIndex;
+  //let value = context.dataset.data[ index ];
+  return index === 3 ||
+    index === 6 ||
+    index === 9 ||
+    index === 12 ||
+    index === 15 ||
+    index === 19 ||
+    index === 22
+    ? 4
+    : 0;
+};
 
-export default function page() {
+const customColor = (context) => {
+  let index = context.dataIndex;
+  //let value = context.dataset.data[ index ];
+  return index === 19 ? 'red' : 'grey';
+};
+export default function fe(props) {
   const classes = style();
   var titulo = { titulo: 'Curva de emprendimiento' };
 
@@ -14,7 +34,37 @@ export default function page() {
           {titulo.titulo}
         </Typography>
 
-
+        <Line
+          data={chartData}
+          options={{
+            legend: {
+              display: false,
+              //position: props.legendPosition
+            },
+            scales: {
+              yAxes: [
+                {
+                  display: false,
+                },
+              ],
+              xAxes: [
+                {
+                  display: false,
+                },
+              ],
+            },
+            layout: {
+              padding: 10,
+            },
+            elements: {
+              point: {
+                radius: customRadius,
+                display: true,
+                backgroundColor: customColor,
+              },
+            },
+          }}
+        />
       </CardContent>
     </Card>
   );
