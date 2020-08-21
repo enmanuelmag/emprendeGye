8d214fcee6ea2d1a235658629308b5d3e41fa2fa
jpvazque <jpvazque@espol.edu.ec>
2020-08-21 14:44:36 -0500

    fix merge conficts

--- src/app/pages/emprendedor/home/components/curva/page.tsx
+++ src/app/pages/emprendedor/home/components/curva/page.tsx
@@ -1,28 +1,20 @@
 import React from 'react';
-import {
-	Card,
-	CardContent,
-	Typography
-} from '@material-ui/core';
+import Image from './image'
+import { Card, CardContent, Typography } from '@material-ui/core';
 import style from './style';
-import Image from "./image";
 
+export default function fe(props) {
+  const classes = style();
+  var titulo = { titulo: 'Curva de emprendimiento' };
 
-
-
-export default function page() {
-	const classes = style();
-	var titulo = {titulo: 'Curva de emprendimiento'}
-
-	return (
-		<Card className={classes.card} variant="outlined">
-			
-			<CardContent  className={classes.content} >
-				<Typography variant="h5" className={classes.titulo} >
-					{titulo.titulo}
-					</Typography>
-				<Image/>	
-			</CardContent>
+  return (
+    <Card className={classes.card} variant="outlined">
+      <CardContent className={classes.content}>
+        <Typography variant="h5" className={classes.titulo}>
+          {titulo.titulo}
+        </Typography>
+        <Image/>	
+    </CardContent>
 		</Card>
 	);
 }
