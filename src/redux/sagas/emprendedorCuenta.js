00a8215980d1fed6b5dc48304fcc8bb858833160
jcoboss <josuealexandercs15@gmail.com>
2020-09-01 16:21:49 -0500

    Add emprendedorCuenta CRUD sagas

--- src/redux/sagas/emprendedorCuenta.js
+++ src/redux/sagas/emprendedorCuenta.js
@@ -1,86 +1,86 @@
 import { put, takeEvery, call, takeLatest } from 'redux-saga/effects';
 import {
-  GET_EMPRENDEDOR_START,
-	GET_EMPRENDEDOR_ERROR,
-	GET_EMPRENDEDOR_COMPLETE,
+	GET_EMPCUENTA_START,
+	GET_EMPCUENTA_ERROR,
+	GET_EMPCUENTA_COMPLETE,
 
-	UPDATE_EMPRENDEDOR_START,
-	UPDATE_EMPRENDEDOR_ERROR,
-	UPDATE_EMPRENDEDOR_COMPLETE,
+	UPDATE_EMPCUENTA_START,
+	UPDATE_EMPCUENTA_ERROR,
+	UPDATE_EMPCUENTA_COMPLETE,
 
-	DELETE_EMPRENDEDOR_START,
-	DELETE_EMPRENDEDOR_ERROR,
-	DELETE_EMPRENDEDOR_COMPLETE,
+	DELETE_EMPCUENTA_START,
+	DELETE_EMPCUENTA_ERROR,
+	DELETE_EMPCUENTA_COMPLETE,
 
-	CREATE_EMPRENDEDOR_START,
-	CREATE_EMPRENDEDOR_ERROR,
-	CREATE_EMPRENDEDOR_COMPLETE
-} from '../actions/emprendedor';
+	CREATE_EMPCUENTA_START,
+	CREATE_EMPCUENTA_ERROR,
+	CREATE_EMPCUENTA_COMPLETE
+} from '../actions/emprendedorCuenta';
 
 import { apiCall } from '../api';
-function* getEmprendedor({ idEmprendedor }) {
+function* getEmprendedorCuenta({ idEmprendedorCuenta }) {
   try {
     const response = yield call(
       apiCall,
-      `/api/emprendedor/${idEmprendedor}`,
+      `/api/emprendedorcuenta/${idEmprendedorCuenta}`,
       null,
       null,
       'GET'
     );
-    yield put({ type: GET_EMPRENDEDOR_COMPLETE, response });
+    yield put({ type: GET_EMPCUENTA_COMPLETE, response });
   } catch (err) {
-    yield put({ type: GET_EMPRENDEDOR_ERROR });
+    yield put({ type: GET_EMPCUENTA_ERROR });
   }
 }
-function* updateEmprendedor(emprendedor) {
+function* updateEmprendedorCuenta(emprendedorCuenta) {
   try {
     //url, data, headers, method
     const response = yield call(
       apiCall,
-      `/api/emprendedor/update/${emprendedor.idEmprendedor}`,
-      emprendedor,
+      `/api/emprendedorcuenta/update/${emprendedorCuenta.idEmprendedor}`,
+      emprendedorCuenta,
       { 'Content-Type': 'application/json' },
       'POST'//POST ES PUT
     );
-    yield put({ type: UPDATE_EMPRENDEDOR_COMPLETE, response });
+    yield put({ type: UPDATE_EMPCUENTA_COMPLETE, response });
   } catch (err) {
-    yield put({ type: UPDATE_EMPRENDEDOR_ERROR });
+    yield put({ type: UPDATE_EMPCUENTA_ERROR });
   }
 }
-function* deleteEmprendedor({ idEmprendedor }) {
+function* deleteEmprendedorCuenta({ idEmprendedorCuenta }) {
   try {
     //url, data, headers, method
     const response = yield call(
       apiCall,
-      `/api/emprendedor/delete/${idEmprendedor}`,
-      emprendedor,
+      `/api/emprendedorcuenta/delete/${idEmprendedorCuenta}`,
+      idEmprendedorCuenta,
       { 'Content-Type': 'application/json' },
       'GET'//GET ES DELETE
     );
-    yield put({ type: DELETE_EMPRENDEDOR_COMPLETE, response });
+    yield put({ type: DELETE_EMPCUENTA_COMPLETE, response });
   } catch (err) {
-    yield put({ type: DELETE_EMPRENDEDOR_ERROR });
+    yield put({ type: DELETE_EMPCUENTA_ERROR });
   }
 },
-function* createEmprendedor(emprendedor) {
+function* createEmprendedorCuenta(emprendedorCuenta) {
   try {
     //url, data, headers, method
     const response = yield call(
       apiCall,
-      `/api/emprendedor/update`,
-      emprendedor,
+      `/api/emprendedorcuenta/update`,
+      emprendedorCuenta,
       { 'Content-Type': 'application/json' },
       'POST'
     );
-    yield put({ type: CREATE_EMPRENDEDOR_COMPLETE, response });
+    yield put({ type: CREATE_EMPCUENTA_COMPLETE, response });
   } catch (err) {
-    yield put({ type: CREATE_EMPRENDEDOR_ERROR });
+    yield put({ type: CREATE_EMPCUENTA_ERROR });
   }
 }
 //Watcher _START
 export default function* teoricos() {
-  yield takeEvery(GET_EMPRENDEDOR_START, getEmprendedor);
-  yield takeLatest(UPDATE_EMPRENDEDOR_START, updateEmprendedor);
-  yield takeLatest(CREATE_EMPRENDEDOR_START, createEmprendedor);
-  yield takeLatest(DELETE_EMPRENDEDOR_START, deleteEmprendedor);
+  yield takeEvery(GET_EMPCUENTA_START, getEmprendedorCuenta);
+  yield takeLatest(UPDATE_EMPCUENTA_START, updateEmprendedorCuenta);
+  yield takeLatest(CREATE_EMPCUENTA_START, createEmprendedorCuenta);
+  yield takeLatest(DELETE_EMPCUENTA_START, deleteEmprendedorCuenta);
 }
