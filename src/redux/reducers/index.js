7853c48317a52c8f55b8dd96008d4ce7488c91fb
jcoboss <josuealexandercs15@gmail.com>
2020-09-01 15:35:15 -0500

    Add emprendedorCuenta CRUD reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -4,6 +4,7 @@ import emprendimiento from './emprendimiento';
 import ganancias from './ganancias';
 import email from './enviarEmail';
 import notifier from './notifier';
+import emprendedorCuenta from "./emprendedorCuenta";
 //import notifier from './notifier';
 
 export const initialState = {
@@ -12,6 +13,7 @@ export const initialState = {
   ganancias,
   email,
   notifier,
+  emprendedorCuenta
 };
 const STORE = combineReducers(initialState);
 
