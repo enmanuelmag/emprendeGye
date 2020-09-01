8c2a3907304cb440b7d0c5f74d4948719431313e
jcoboss <josuealexandercs15@gmail.com>
2020-09-01 15:32:24 -0500

    Add emprendedorCuenta crud actions

---
+++ src/redux/actions/emprendedorCuenta.js
@@ -0,0 +1,31 @@
+export const GET_EMPCUENTA_START = 'GET_EMPCUENTA_START'
+export const GET_EMPCUENTA_ERROR = 'GET_EMPCUENTA_ERROR'
+export const GET_EMPCUENTA_COMPLETE = 'GET_EMPCUENTA_COMPLETE'
+
+export const UPDATE_EMPCUENTA_START = 'UPDATE_EMPCUENTA_START'
+export const UPDATE_EMPCUENTA_ERROR = 'UPDATE_EMPCUENTA_ERROR'
+export const UPDATE_EMPCUENTA_COMPLETE = 'UPDATE_EMPCUENTA_COMPLETE'
+
+export const CREATE_EMPCUENTA_START = 'CREATE_EMPCUENTA_START'
+export const CREATE_EMPCUENTA_ERROR = 'CREATE_EMPCUENTA_ERROR'
+export const CREATE_EMPCUENTA_COMPLETE = 'CREATE_EMPCUENTA_COMPLETE'
+
+export const DELETE_EMPCUENTA_START = 'DELETE_EMPCUENTA_START'
+export const DELETE_EMPCUENTA_ERROR = 'DELETE_EMPCUENTA_ERROR'
+export const DELETE_EMPCUENTA_COMPLETE = 'DELETE_EMPCUENTA_COMPLETE'
+
+//Son llamadas por los componentes
+export const getEmprendedorCuenta = ({idEmprendedorC}) => ({
+    type: GET_EMPCUENTA_START, idEmprendedorC
+})
+export const deleteEmprendedorCuenta = ({idEmprendedorC}) => ({
+    type: DELETE_EMPCUENTA_START, idEmprendedorC
+})
+
+export const updateEmprendedorCuenta = (emprendedorC) => ({
+    type: UPDATE_EMPCUENTA_START, emprendedorC
+})
+
+export const createEmprendedorCuenta = (emprendedorC) => ({
+    type: CREATE_EMPCUENTA_START, emprendedorC
+})
--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -3,7 +3,7 @@ import emprendedor from './emprendedor';
 import emprendimiento from './emprendimiento';
 import ganancias from './ganancias';
 import email from './enviarEmail';
-
+import emprendedorCuenta from "./emprendedorCuenta";
 export default function* rootSaga() {
-  yield all([emprendedor(), emprendimiento(), ganancias(), email()]);
+  yield all([emprendedor(), emprendimiento(), ganancias(), email(), emprendedorCuenta()]);
 }
