a0601332b03a6e67cef71c9e132b07922e9dc616
enmanuel-mag <fmagalla@fiec.espol.edu.ec>
2020-09-16 11:18:16 -0500

    Coonect coach class per year with graph in dashboard

--- src/app/pages/emprendedor/home/components/graficoAsesoriasAnio/data.js
+++ src/app/pages/emprendedor/home/components/graficoAsesoriasAnio/data.js
@@ -1,13 +1,17 @@
 export default {
-        labels: [2017, 2018, 2019],
-        datasets: [
-            {
-                label: "Mentor: Luis Gutierrez",
-                type: "bar",
-                stack: "Base",
-                backgroundColor: "#57BC82",
-                hoverBackgroundColor: "#00CD57",
-                data: [ 30, 48, 36 ],
-            }
-        ]
+  labels: [2017, 2018, 2019],
+  datasets: [
+    {
+      label: 'Mentor: Luis Gutierrez',
+      type: 'bar',
+      stack: 'Base',
+      backgroundColor: '#57BC82',
+      hoverBackgroundColor: '#00CD57',
+      data: [
+        Math.random() * (50 - 20) + 20,
+        Math.random() * (70 - 10) + 10,
+        Math.random() * (80 - 30) + 30,
+      ],
+    },
+  ],
 }
